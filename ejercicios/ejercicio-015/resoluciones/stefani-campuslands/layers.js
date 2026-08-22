145a139198714e801254a2b00ef72ee31d47b5e9
steff-360 <steff.18random@gmail.com>
2026-08-21 20:47:23 -0600

    fix(ejercicio-015): corrige suma de puntos y orden del ranking

---
+++ ejercicios/ejercicio-015/resoluciones/stefani-campuslands/layers.js
@@ -0,0 +1,7 @@
+﻿export function calcularResultado(datos) {
+  return datos.reduce((suma, item) => suma + item.puntos, 0);
+}
+
+export function ordenarRanking(jugadores) {
+  return [...jugadores].sort((a, b) => b.puntos - a.puntos);
+}
